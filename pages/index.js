c6254994e4eb15f33e23a78e130c613a51761452
sesobeso77 <sesobeso77@gmail.com>
2026-03-17 17:17:21 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding: "50px", fontFamily: "Arial"}}>
+      <h1>KMG Financial LLC</h1>
+      <p>Your website is live 🎉</p>
+    </div>
+  );
+}
